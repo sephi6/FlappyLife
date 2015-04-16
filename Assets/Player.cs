a989d5b059350824e3c401e5afa45c41b8d043a6
sephi6 <vdominguezaragon@gmail.com>
2015-04-16 17:33:11 +0200

    pajarito con movimiento

---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public Vector2 force = new Vector2(0, 1);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidbody2D.velocity = force;
+        }
+        foreach (Touch i in Input.touches){
+            if(i.phase==TouchPhase.Began){
+                rigidbody2D.velocity = force;
+            }
+        }
+
+        rigidbody2D.rotation = rigidbody2D.velocity.y * 6;
+	}
+}
