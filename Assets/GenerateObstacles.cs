72b04b90f3a22cd93f144b80b9856b03203358a0
sephi6 <vdominguezaragon@gmail.com>
2015-04-18 13:23:53 +0200

    obstáculos, movimiento y generación

---
+++ Assets/GenerateObstacles.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateObstacles : MonoBehaviour {
+
+    public GameObject obstacle;
+
+    public Vector2 position = new Vector2(0, 0);
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("instanceObstacle", 1f, 1.5f);
+	}
+	
+	// Update is called once per frame
+    void instanceObstacle()
+    {
+        Instantiate(obstacle);
+    }
+}
---
+++ Assets/obstacles.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class obstacles : MonoBehaviour {
+
+    public Vector2 velocity = new Vector2(4, 0);
+
+    public int score;
+
+    public Text text;
+
+    public float range = 4;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.rigidbody2D.velocity = velocity;
+        gameObject.transform.position = new Vector2(transform.position.x, transform.position.y - range * Random.value);
+	}
+	
+	// Update is called once per frame
+	
+    void OnTriggerEnter(Collider2D other){
+        score += 1;
+        text.text = "Score:" + score;
+       
+    }
+}
