0ca05df6cf75e66162fac244e2799a4f194a2563
sephi6 <vdominguezaragon@gmail.com>
2015-05-18 18:22:21 +0200

    facebook optimizado y fondo e iconos

--- Assets/fotos.cs
+++ Assets/fotos.cs
@@ -1,20 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class fotos : MonoBehaviour {
 
 	// Use this for initialization
+    public static Dictionary<string, Texture> fotosGuardadas= new Dictionary<string,Texture>();
+
     void Start()
     {
 		if (FB.IsLoggedIn) {
 			int random = Random.Range (0, FBHolder.urls.Count);
-			if (FBHolder.urls.Count != 0) {
-				StartCoroutine (cargaString (FBHolder.urls [random]));
-			} else {
+            if (FBHolder.urls.Count != 0 && !fotosGuardadas.ContainsKey(FBHolder.urls[random]))
+            {
+
+                StartCoroutine(cargaString(FBHolder.urls[random]));
+                
+
+            }
+            else if (FBHolder.urls.Count != 0)
+            {
+                this.renderer.material.mainTexture=fotosGuardadas[FBHolder.urls[random]];
+                Debug.Log("Foto reutilizada");
+            }else{
+            
 				Debug.LogWarning ("No tenemos url");
 			}
+    }
 		}
-	}
+	
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -29,7 +44,10 @@ public class fotos : MonoBehaviour {
         yield return respuesta;
         //ya tenemos la url, podemos hacer con ella lo que queramos
         //this.stringAModificar = respuesta.text;
+        fotosGuardadas.Add(url, respuesta.texture);
+        //fotosGuardadas[url] = respuesta.texture;
         this.renderer.material.mainTexture = respuesta.texture;
-        Debug.Log("FOTO CARGADA");
+        Debug.Log("FOTO CARGADA: " + fotosGuardadas.Keys.Count);
     }
 }
+
